686fedd6c681f4969e5eaa37513a5b495e58b7ac
britoalana <110117693+britoalana@users.noreply.github.com>
2023-05-18 10:48:52 -0300

    Update app.js

--- sumario/sumario/app.js
+++ sumario/sumario/app.js
@@ -37,14 +37,18 @@ const itens = [
     let displayMenu = menuItems.map(function (item, index) {
       const color=['#FFF6F5','#FFFBF2','#F2FBFA','#F3F3FD']
       const bgColor = color[index % color.length];
-    
+      const colors=['hsl(0, 100%, 67%)','hsl(39, 100%, 56%)','hsl(166, 100%, 37%)','hsl(234, 85%, 45%)']
+      const Color = colors[index % colors.length];
       return `
       <article class="menu-item" style=" background-color: ${bgColor};" >
       <div>
       <img src="${item.icon}" alt="">
-      <p>${item.category} ${item.score}/100</p>
+      <p class="categoria" style=" color: ${Color};" >${item.category}</p> <p>${item.score}/100</p>
     </div></article>`
 
+  
+      //retorna todas essas informações de todos os objetos 
+    })
   
       //retorna todas essas informações de todos os objetos 
     })
